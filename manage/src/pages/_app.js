d752cc0169206530c6602dc5eeff4b043303be97
master1lan <278457198@qq.com>
2022-07-30 15:50:15 +0800

    Update _app.js

--- manage/src/pages/_app.js
+++ manage/src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@styles/globals.css';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
+// import 'antd/dist/antd.dark.css';
+// import 'antd/dist/antd.compact.css';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
