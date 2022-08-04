ba44b6578c0f9493ea5497cb63d3a100c6b9fd4f
chunhai <815818600@qq.com>
2022-08-04 22:42:33 +0800

    抽离

--- example/src/core/index.ts
+++ example/src/core/index.ts
@@ -2,6 +2,6 @@ import './src/performance/index'
 import './src/http/index'
 // type: 'Error' | 'Performance' | 'User' | 'Request'
 // name: string
-// data: performance | user
+// data: performance | user | 'Request'
 console.log('monitor plugin installed')
 
