7a1d09ff8d57650cf45b0e70ab59fe7b2d448324
Octane0411 <wdznb1@gmail.com>
2022-08-06 20:25:02 +0800

    fix getPlatform

--- example/src/core/src/utils/user.ts
+++ example/src/core/src/utils/user.ts
@@ -25,8 +25,29 @@ export const getBrowserInfo = () => {
 }
 
 export const getPlatform = () => {
-    // @ts-ignore
-    return window.navigator.userAgentData.platform
+    //@ts-ignore
+    let userAgentData = window.navigator.userAgentData
+    let userAgent = window.navigator.userAgent,
+        // @ts-ignore
+        platform = userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = "";
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } else if (/Linux/.test(platform)) {
+        os = 'Linux';
+    }
+
+    return os;
 }
 
 
